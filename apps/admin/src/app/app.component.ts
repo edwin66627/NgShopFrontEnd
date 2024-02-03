c36e9d3fa7b2a987bea8d439097eb6f9d01ec64b
Edwin Erley Quevedo Sierra <edwinqs1956@hotmail.com>
2024-02-03 15:57:06 -0500

    Structured style files

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mycompany-root',
+  selector: 'admin-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'admin';
--- apps/ecommerce-ng-shop/src/app/shared/header/header.component.ts
+++ apps/ecommerce-ng-shop/src/app/shared/header/header.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ng-shop-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {}
