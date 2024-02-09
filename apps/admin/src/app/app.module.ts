8e52a786560dd18dec8c1ce5261988698a386bd5
Edwin Erley Quevedo Sierra <edwinqs1956@hotmail.com>
2024-02-08 22:03:02 -0500

    Added basic Categories component Includes 'New'button and table Not fetching categories data from backend

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -8,19 +8,36 @@ import { appRoutes } from './app.routes';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+
+const UX_MODULE = [
+  ButtonModule,
+  CardModule,
+  TableModule,
+  ToastModule,
+  ToolbarModule,
+]
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent],
+  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    ...UX_MODULE
   ],
   providers: [],
   bootstrap: [AppComponent],
   exports: [
     DashboardComponent,
     ShellComponent,
-    SidebarComponent
+    SidebarComponent,
+    CategoriesListComponent,
   ],
 })
 export class AppModule {}
